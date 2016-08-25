e0e570b88efee2001b9f9f07b781973fce59ce86
Kelly O'Neil <kelly.brianne.oneil@gmail.com>
2016-08-24 19:50:35 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(value){
+    return value+2;
+}
+
+function multiply(value){
+    return value*3;
+}
+
+function subtract(value){
+    return value-4;
+}
+
+console.log(add(multiply(subtract(10))));
